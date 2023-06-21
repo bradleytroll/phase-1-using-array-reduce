ad92b1c8f0e1daafdcc643b3e8f66395de73d74e
Bradley Troll <bradleytroll@gmail.com>
2023-06-21 10:11:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+//Redoing old labs for extra practice.
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+//Arrow function:
+// const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
-
-const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+const totalBatteries = batteryBatches.reduce(function(total, location){
+    return total + location;
+}, 0);
